ee929d7e73b7108d7a5a66540bf87ccfe2c9f595
Tomás <tomasg92@gmail.com>
2025-03-12 18:48:10 -0400

    chore(static-assets): updates static served filed

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,10 @@ app.use(cors())
 // Serve frontend static files
 
 if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.join(__dirname, '../frontend/dist')
-
-  app.use(express.static(frontendPath))
+  app.use(express.static('frontend/dist'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   })
 }
 
