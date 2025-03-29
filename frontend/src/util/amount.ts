945bb51345fa70b2c37c10e35c5f9914eeb122d7
Tom <tomasg@gmail.com>
2025-03-29 13:38:31 -0400

    feat(amounts): adds locale strings

--- frontend/src/util/amount.ts
+++ frontend/src/util/amount.ts
@@ -1,6 +1,10 @@
 function amount(number: number) {
   if (number === null) return '0.00'
-  return `${number.toFixed(2)}`
+
+  return number.toLocaleString('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })
 }
 
 export default amount
